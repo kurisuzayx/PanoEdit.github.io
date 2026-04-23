d6de935289e6165b89384c587e58bc06680966aa
栗悟饭とカメハメ波 <zhouyuxi.abc@gmail.com>
2026-04-23 21:55:06 +0800

    Add PanoEdit project page

---
+++ README.md
@@ -0,0 +1,7 @@
+# PanoEdit Project Page
+
+This directory contains the standalone project website for the IJCV paper:
+
+`PanoEdit: Fine-grained Control for Consistent 360 Panorama Editing`
+
+The page is adapted from the `FDSM.github.io` template and is ready to be used as a GitHub Pages site.
---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burgers = Array.from(document.querySelectorAll(".navbar-burger"));
+  burgers.forEach((burger) => {
+    burger.addEventListener("click", () => {
+      const target = burger.dataset.target;
+      const menu = document.getElementById(target);
+      burger.classList.toggle("is-active");
+      if (menu) {
+        menu.classList.toggle("is-active");
+      }
+    });
+  });
+});
